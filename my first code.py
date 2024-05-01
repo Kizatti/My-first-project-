13523e4e0e5dab591cf4298996e0768b8549bf2e
Kizatti <kaybee437@gmail.com>
2024-05-01 18:32:39 +0100

    first commit

---
+++ my first code.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  3 21:32:59 2023
+
+@author: KAYBEE
+"""
+
+import pandas as pd
+covid_data = pd.read_excel("C:\\Users\TIFEM TECHNOLOGY\Desktop\Covid Data.xlsx")
+covid_data.columns
+covid_data
+2+2
---
+++ myscript.js
@@ -0,0 +1,5 @@
+console.log("Hello from myscript.js");
+console.log("Hello again");
+function myfunction(x) {
+    x.classList.toggle("change");
+}
